c346e696017cb176dba09168aa444ebf6831513b
fulprimitiv <eremeevdima51@gmail.com>
2024-06-13 20:08:04 +0500

    фикс накосячил с вьюшкой

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,43 +1,8 @@
-import { createPointTemplate } from '../templates/point-template.js';
-import AbstractView from '../framework/view/abstract-view';
-export default class PointView extends AbstractView{
-  #point;
-  #offers;
-  #destination;
-  #onPointEditFormClick;
-  #onFavoriteClick;
+import { createSortTemplate } from '../templates/sort-template.js';
+import AbstractRadioListView from './abstract-radio-list-view.js';
 
-  constructor({point, offers, destination, onPointEditFormClick, onFavoriteClick}) {
-    super();
-    this.#point = point;
-    this.#offers = offers;
-    this.#destination = destination;
-    this.#onPointEditFormClick = onPointEditFormClick;
-    this.#onFavoriteClick = onFavoriteClick;
-
-    this.#addPointHandlers();
-  }
-
+export default class SortView extends AbstractRadioListView{
   get template() {
-    return createPointTemplate(this.#point, this.#offers, this.#destination);
+    return createSortTemplate({sorts: this.items});
   }
-
-  #addPointHandlers = () => {
-    this.element
-      .querySelector('.event__rollup-btn')
-      .addEventListener('click', this.#editClickHandler);
-    this.element
-      .querySelector('.event__favorite-icon')
-      .addEventListener('click', this.#favoriteClickHandler);
-  };
-
-  #editClickHandler = (evt) => {
-    evt.preventDefault();
-    this.#onPointEditFormClick();
-  };
-
-  #favoriteClickHandler = (evt) => {
-    evt.preventDefault();
-    this.#onFavoriteClick();
-  };
 }
